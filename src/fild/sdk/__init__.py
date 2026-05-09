819f229d6775de500b94c02cce4fa7c3f250262d
Elena Kulgavaya <elena.kulgavaya@gmail.com>
2026-05-09 17:16:48 +0200

    Migrate to surety

--- src/fild/sdk/__init__.py
+++ src/fild/sdk/__init__.py
@@ -1,3 +1,14 @@
+import warnings
+
+warnings.warn(
+    "fild is deprecated and will no longer be maintained. "
+    "Migrate to surety: pip install surety. "
+    "Replace 'from fild.sdk import ...' with 'from surety import ...'. "
+    "See https://github.com/elenakulgavaya/surety for details.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 from .array import Array, Set
 from .field import Field
 from .dictionary import Dictionary
